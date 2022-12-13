d58a8bda709828687ae5c285a8df29e817e8d6e9
kelapacuyy <86961194+kelapacuyy@users.noreply.github.com>
2022-12-13 08:42:35 +0700

    Update registrasi.php

--- registrasi.php
+++ registrasi.php
@@ -70,7 +70,7 @@ if( isset($_POST['register']) ){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DapurKost</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <link href="style.css" rel="stylesheet">
+    <link href="css/registrasiStyle.css" rel="stylesheet">
     <style>
         /* Chrome, Safari, Edge, Opera */
         input::-webkit-outer-spin-button,
